bfbf20e226ab7541cd35fa3366c2f1463a4ba0a6
Debjyoti Chakraborty <124529135+debjyotichakraborty@users.noreply.github.com>
2023-05-14 14:28:02 +0530

    Copied from desktop analytics_py_learning.py

--- analytics_py_learning.py
+++ analytics_py_learning.py
@@ -1,2 +1,20 @@
-a=5
-a
+import numpy as np
+imprt pandas as pd
+import os
+for dirname, _, filenames in os.walk('/kaggle/input'):
+  for filename in filenames:
+    print(os.path.join(dirname,filename))
+telecom = pd.read_csv('/kaggle/input/telecom-customer-churn-by-maven-analytics/telecom_customer_churn.csv')
+telecom
+telecom.info()
+telecom.groupby('Customer Status')['Monthly Charge'].describe()
+telecom.groupby('Customer Status')['Total Charges'].describe()
+telecom.describe().T
+telecom.groupby(['Customer Status','Married','Gender'])['Monthly Charge'].describe()
+telecom.groupby(['Customer Status'])['Avg Monthly GB Download'].describe()
+import seaborn as sbn
+sbn.lineplot(x='Tenure in Months',y='Monthly Charge', data=telecom)
+sbn.lineplot(x='Avg Monthly GB Download',y='Total Charges', data=telecom)
+sbn.lineplot(x='Avg Monthly GB Download',y='Total Extra Data Charges',data=telecom)
+sbn.countplot(x='Customer Status',data=telecom)
+sbn.boxplot(x='Customer Status',y='Monthly Charge',data=telecom)
